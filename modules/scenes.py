9938a44fb4c0ee660ee54db1a6a131b40e179922
DewasSquid <yohansambin@gmail.com>
2024-02-10 17:58:21 -0300

    moved level to scene

--- modules/scenes.py
+++ modules/scenes.py
@@ -1,5 +1,9 @@
+import os
+
 from ursina import *
-from modules.visuals import BackgroundImage, Textures
+
+from modules.custom_entities import *
+from modules.visuals import *
 
 
 class Scene(Entity):
@@ -8,11 +12,53 @@ class Scene(Entity):
         super().__init__(*args, **kwargs)
         self.enabled = False
 
+class Level(Scene):
+    def __init__(self, level_path: str, *args, **kwargs) -> None:
+        """Enregistre un niveau à partir d'une structure hiérarchisée précise
+
+        Args:
+            level_path (str): chemin d'accès à la structure hiérarchisée
+        
+        Examples:
+        ```py
+        GenerateLevel(level_path="./levels/level_perso")
+        ```
+        
+        ```
+        ./levels/level_perso
+        |---> thumbnail.png
+        |---> about.txt
+        |---> ./assets/
+              |---> fond.fichier_visuel
+              |---> ./audio/
+                    |---> fichiers_audio.wav
+              |---> ./models/
+                    |---> models_pour_les_cubes.obj
+        """
+        super().__init__(*args, **kwargs)
+        self.__level_path = Path(level_path)
+        self.data = self.__create_hierarchy(self.__level_path)
+
+    def __create_hierarchy(self, path: str) -> dict:
+        """Génère la liste des assets du niveau
+
+        Args:
+            path (str): Le chemin d'accès au dossier du niveau 
+
+        Returns:
+            dict: La liste des fichiers et sous-dossiers du niveau, automatiquement accédée par le jeu
+        """
+        data = {}
+        for item in os.walk(path):
+            pass
+
 class MainMenu(Scene):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
     
-        BackgroundImage(load_texture(Textures.MENU_BACKGROUND))
+        BackgroundImage(
+            load_texture(Textures.MENU_BACKGROUND),
+        )
 
         menu_buttons = {
             "Jouer": Func(None),
