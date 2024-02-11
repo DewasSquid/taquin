92c5f3217de6982322d8efadda7b176692bcaa2f
DewasSquid <yohansambin@gmail.com>
2024-02-10 23:47:31 -0300

    added bloom shader

--- modules/visuals.py
+++ modules/visuals.py
@@ -2,10 +2,16 @@ from ursina import *
 
 
 class Shaders:
-    """Une énumération de chemin d'accès vers des shaders"""
-    pass
+    """Une énumération de fonctions pour shaders"""
+    def bloom(blur_size: int = .05):
+        return Shader(
+            "Bloom",
+            fragment=open("./assets/shaders/bloom.frag").read(),
+            default_input={
+                "blur_size": blur_size
+            }
+        )
 
 class Textures:
     """Une énumération de chemin d'accès vers des textures"""
     MENU_BACKGROUND = "./assets/images/menu_bg.jpg"
-    MENU_BG = MENU_BACKGROUND  # Alias
