b22939ab94c490d783e07a1ff4fa553ef5414cbd
DewasSquid <yohansambin@gmail.com>
2024-02-13 21:29:05 -0300

    added level class

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,2 @@
-from modules.custom_entities import BackgroundImage, MenuButton
+from modules.custom_entities import BackgroundImage, MenuButton, Level
 from modules.visuals import Shaders, Textures
