aba520909839494172a0516c8cb90baa373641aa
DewasSquid <yohansambin@gmail.com>
2024-04-17 19:58:50 -0300

    changed the code to english

--- modules/level.py
+++ modules/level.py
@@ -1,18 +1,17 @@
 import itertools
 import math
 import random
-
+from typing import List
 from ursina import *
-
 
 class Brick(Button):
     def __init__(self, model: Mesh, id: int, level, *args, **kwargs) -> None:
-        """Entitée représentant une brique de tableau
+        """Entity representing a brick in the board
 
         Args:
-            model (Mesh): Le modèle de la brique
-            id (int): Son identifiant la représentant dans un tableau
-            level (level): Le niveau parent
+            model (Mesh): The model of the brick
+            id (int): Unique identifier for the brick
+            level (Level): The parent level
         """
         super().__init__(
             model=model,
@@ -23,14 +22,13 @@ class Brick(Button):
         
         self.id = id
         self.level = level
-        
         if self.id == 0:
             self.color = color.rgba(0, 0, 0, 0)
             self.highlight_color = self.color
             self.pressed_color = self.color
 
     def on_click(self) -> None:
-        """Échange la position de cette brique avec la brique noire lorsqu'elle est cliquée"""
+        """Swaps the position of this brick with the black brick when clicked"""
         if self == self.level.black_brick: return
 
         x1, y1, _ = self.position
@@ -42,11 +40,11 @@ class Brick(Button):
 class Level(Entity):
     MIN_BRICKS = 9
     
-    def __init__(self, models: list[Mesh], *args, **kwargs) -> None:
-        """Structure pour un niveau de base
+    def __init__(self, models: List[Mesh], *args, **kwargs) -> None:
+        """Structure for a basic level
         
         Args:
-            models (list[Mesh]): Une liste de tous les modèles de briques présents dans le niveau
+            models (List[Mesh]): List of all the brick models in the level
         """
         super().__init__(*args, **kwargs)
         
@@ -57,11 +55,11 @@ class Level(Entity):
         self.dimension = math.ceil(math.sqrt(self.model_amount))
         
         self.models = models
-        self.black_brick = None
+        self._black_brick = None
 
     def setup_camera(self) -> None:
-        """Définit la position de la caméra en fonction du nombre de briques"""
-        # Merci à cet article qui m'as permis de trouver cette formule !
+        """Sets camera position based on the number of bricks"""
+        # Thanks to this article for the formula!
         # https://discourse.threejs.org/t/camera-zoom-to-fit-object/936
         
         scale_factor = max(1, self.model_amount / self.MIN_BRICKS)
@@ -76,16 +74,42 @@ class Level(Entity):
         camera.position = Vec3(center_x, center_y, -camera_distance)
 
     @property
-    def bricks(self) -> list[Brick]:
-        """Renvoie une liste de toutes les briques présentes dans le niveau"""
+    def black_brick(self) -> Brick:
+        """Returns the black brick"""
+        return self._black_brick
+
+    @black_brick.setter
+    def black_brick(self, brick: Brick) -> None:
+        """Sets the black brick"""
+        self._black_brick = brick
+
+    @property
+    def bricks(self) -> List[Brick]:
+        """Returns a list of all bricks in the level"""
         bricks = []
         for row in self.board:
             for brick in row:
                 bricks.append(brick)
         return bricks
     
+    @property
+    def board(self) -> List[List[Brick]]:
+        """Returns the current state of the game board"""
+        board = [[None for _ in range(self.dimension)] for _ in range(self.dimension)]
+        for row in range(self.dimension):
+            for column in range(self.dimension):
+                for brick in self.parent.children:
+                    if brick.position.x == row and brick.position.y == column:
+                        board[row][column] = brick
+        return board
+
+    @board.setter
+    def board(self, new_board: List[List[Brick]]) -> None:
+        """Sets the state of the game board"""
+        self._board = new_board
+
     def create(self) -> None:
-        """Séquence pour créé un niveau complet, avec tableau et séquence d'attente"""
+        """Sequence to create a complete level, with board and wait sequence"""
         self.setup_camera()
         
         board_seq = Sequence(
@@ -99,7 +123,7 @@ class Level(Entity):
         board_seq.start()
 
     def generate_board(self) -> None:
-        """Génère un tableau de taquin en fonction du nombre de modèles"""
+        """Generates a board based on the number of models"""
         column = []
         i = 0
         for x in range(self.dimension):
@@ -127,33 +151,48 @@ class Level(Entity):
         self.board = column
 
     def shuffle_board(self) -> None:
-        """Mélange les briques du tableau"""
-        # Collecte les positions originales des briques dans la grille
+        """Shuffles the bricks on the board"""
+        # Collect original positions of bricks on the grid
         original_positions = {(brick.position.x, brick.position.y): brick for brick in self.bricks}
 
-        # Génère un nouvel ordre aléatoire pour les positions des briques dans la grille
+        # Generate a new random order for brick positions on the grid
         random_positions = list(original_positions.keys())
         random.shuffle(random_positions)
 
-        # Réaffecte les briques aux positions aléatoires dans la grille
+        # Reassign bricks to random positions on the grid and update the board
+        new_board = [[None for _ in range(self.dimension)] for _ in range(self.dimension)]
         for (x1, y1), (x2, y2) in zip(original_positions.keys(), random_positions):
-            original_positions[(x1, y1)].animate_position(Vec3(x2, y2, 1), duration=.15)
+            brick = original_positions[(x1, y1)]
+            brick.animate_position(Vec3(x2, y2, 1), duration=.15)
+            new_board[int(x2)][int(y2)] = brick
+
+        self.board = new_board
 
     def swap_bricks(self, brick1: Brick, brick2: Brick) -> None:
-        """Échange les positions de deux briques"""
+        """Swaps the positions of two bricks"""
         brick1_position, brick2_position = brick1.position, brick2.position
         brick1.animate_position(value=brick2_position, duration=.05, curve=curve.linear)
         brick2.animate_position(value=brick1_position, duration=.05, curve=curve.linear)
-        
+
+        # Update the board immediately after swapping the bricks
+        self._board = self.generate_board_state()
+
         print(self.is_solved())
-    
+
+    def generate_board_state(self) -> List[List[Brick]]:
+        """Generates the current state of the game board"""
+        board = [[None for _ in range(self.dimension)] for _ in range(self.dimension)]
+        for row in range(self.dimension):
+            for column in range(self.dimension):
+                for brick in self.parent.children:
+                    if brick.position.x == row and brick.position.y == column:
+                        board[row][column] = brick
+        return board
+
     def is_solved(self) -> bool:
-        """Vérifie si le tableau est résolu"""
-        # FIXME: this function always returns true. Might be caused by the board variable being technically constant
-        i = 0
-        for x, y in itertools.product(range(self.dimension), repeat=2):
-            brick : Brick = self.board[x][y]
-            print(brick.id, i)
-            if brick.id != i: return False
-            i += 1
+        """Verifies if the board is solved"""
+        flattened_board = [brick.id for row in self.board for brick in row if brick]
+        for i, brick_id in enumerate(flattened_board):
+            if brick_id != i:
+                return False
         return True
